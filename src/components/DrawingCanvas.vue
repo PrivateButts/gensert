e2ca79835aad93f38c0692f13e649845de92dd76
Randy Butts <rbutts@chq.org>
2025-05-30 00:09:16 -0400

    Added shape drawing

--- src/components/DrawingCanvas.vue
+++ src/components/DrawingCanvas.vue
@@ -4,7 +4,7 @@
             <fieldset class="fieldset bg-base-100 border-base-300 rounded-box w-64 border p-4">
                 <legend class="fieldset-legend">Tool Selection</legend>
                 <label class="label">
-                    <input type="radio" v-model="activeTool" value="scale" class="radio" />
+                    <input type="radio" v-model="activeTool" value="scale" class="radio" @change="redrawStage" />
                     Scale
                 </label>
                 <label class="label">
@@ -14,23 +14,29 @@
             </fieldset>
 
             <input type="number" class="input" min="1" v-model="scaleLineDistance" />
+            <input type="range" class="input" min="0" max="1" step=".01" v-model="outlineTension">
         </div>
-        <v-stage :config="configKonva">
+        <v-stage :config="configKonva" ref="stage" @click="dispatchClick">
             <v-layer>
                 <v-image v-if="image" :config="configImage" />
             </v-layer>
-            <v-layer v-if="activeTool === 'scale'">
+            <v-layer>
                 <v-line :config="configScaleLine"></v-line>
+            </v-layer>
+            <v-layer v-if="outlinePoints.length > 1">
+                <v-line :config="configOutline"></v-line>
             </v-layer>
         </v-stage>
     </div>
 </template>
 
 <script setup>
-import { ref, onMounted } from 'vue'
+import { ref, onMounted, reactive, computed } from 'vue'
 import { useImage } from 'vue-konva'
 
 const props = defineProps(['image'])
+
+const stage = ref(null)
 
 const configKonva = ref({
     width: window.innerWidth * .9,
@@ -51,8 +57,12 @@ const activeTool = ref('scale')
 const imageWidth = ref(0)
 const imageHeight = ref(0)
 
-const scaleLinePoints = ref([])
+const scaleLinePoints = ref([0, 0, 0, 0])
+let scalePntSetA = true
 const scaleLineDistance = ref(10)
+
+const outlinePoints = ref([])
+const outlineTension = ref(0)
 
 const configImage = ref({
     x: 0,
@@ -62,11 +72,36 @@ const configImage = ref({
     height: imageHeight
 })
 
-const configScaleLine = ref({
-    points: scaleLinePoints,
-    stroke: "orange",
-    strokeWidth: 2
+const configScaleLine = computed(() => {
+    return {
+        x: 0,
+        y: 0,
+        points: scaleLinePoints.value,
+        stroke: activeTool.value === 'scale' ? "orange" : "gray",
+        tension: 0,
+        closed: false,
+    }
 })
+
+const canShowScaleLine = computed(() => {
+    return scaleLinePoints.value.every(pnt => pnt !== 0)
+})
+
+const configOutline = computed(() => {
+    return {
+        x: 0,
+        y: 0,
+        points: outlinePoints.value,
+        stroke: activeTool.value === 'outline' ? "blue" : "gray",
+        tension: parseFloat(outlineTension.value),
+        closed: true,
+    }
+})
+
+
+function redrawStage() {
+    stage.value.getNode().batchDraw()
+}
 
 
 onMounted(() => {
@@ -101,6 +136,45 @@ async function calcImageContain(imgBlob) {
         img.onerror = reject;
         img.src = imgBlob
     })
+}
+
+function addScaleLinePoint(event) {
+    console.debug('addScaleLinePoint', event)
+    const mousePos = stage.value.getNode().getPointerPosition();
+    if (scalePntSetA) {
+        scaleLinePoints.value[0] = mousePos.x
+        scaleLinePoints.value[1] = mousePos.y
+    } else {
+        scaleLinePoints.value[2] = mousePos.x
+        scaleLinePoints.value[3] = mousePos.y
+    }
+    scalePntSetA = !scalePntSetA
+
+    redrawStage()
+}
+
+
+function addOutlinePoint(event) {
+    console.debug('addOutlinePoint', event)
+    const mousePos = stage.value.getNode().getPointerPosition();
+
+    outlinePoints.value.push(mousePos.x, mousePos.y)
+    redrawStage()
+}
+
+
+function dispatchClick(event) {
+    switch (activeTool.value) {
+        case 'scale':
+            addScaleLinePoint(event)
+            break;
+        case 'outline':
+            addOutlinePoint(event)
+            break;
+        default:
+            console.warn("Unknown tool")
+            break;
+    }
 }
 
 
